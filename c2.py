f36d276cdcfc02ab3df5dfb0813694318628da96
RzLModz <97414556+RzLModz@users.noreply.github.com>
2024-07-28 22:01:07 +0700

    Add files via upload

--- c2.py
+++ c2.py
@@ -327,9 +327,9 @@ def main():
                 bytes = cnc.split()[3]
                 thrs = cnc.split()[4]
                 bost = cnc.split()[5]
-                os.system(f'python2 brutal.py {ip} {port} {bytes} {thrs} {bost}')           
+                os.system(f'python3 brutal.py {ip} {port} {bytes} {thrs} {bost}')           
             except IndexError:    
-                print('Example: 1.1.1.1 80 500 500 Y/N')
+                print('Example: brutal 1.1.1.1 80 500 500 Y/N')
                 
  # AMP/GAMES METHODS
 
